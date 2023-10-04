bcf26eb11f5a7302eeecf4c1cfb34e2b27836471
devopsandes <gonzalomorresi@gmail.com>
2023-10-04 14:35:31 -0300

    cambio de url de envío de formulario.

--- src/pages/Contacto.jsx
+++ src/pages/Contacto.jsx
@@ -113,8 +113,8 @@ function Contacto() {
 
         console.log("este es el dataFormulario--->", dataFormulario);
         const response = await axios.post(
-          /* "https://35.164.170.212:443/api/submit-contactanos" */
-          "http://localhost:443/api/submit-formulario",
+          "https://35.164.170.212:443/api/submit-contactanos"
+          /* "http://localhost:443/api/submit-formulario" */,
           dataFormulario
         );
 
